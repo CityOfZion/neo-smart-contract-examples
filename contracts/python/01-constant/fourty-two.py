295ac05a5ea80064790eb597d74ec86999274780
Travis Lin <travis@localhost.dev>
2018-02-27 09:52:18 +1100

    Example contracts to get the project started

---
+++ contracts/python/01-constant/fourty-two.py
@@ -0,0 +1,13 @@
+
+"""
+Test Command:
+    build ./contracts/python/01-constant/fourty-two.py test ff 02 True True
+
+Import Command:
+    import contract ./contracts/python/01-constant/fourty-two.py ff 02 True True
+
+Example Invocation:
+    testinvoke 7eda0d8656310d97d6b342cd831f8d1be7186ee8
+"""
+def Main():
+    return 42
---
+++ contracts/python/02-number/add.py
@@ -0,0 +1,13 @@
+def Main(a, b):
+    """
+    :param a: an integer
+    :param b: an integer
+    :return: a + b
+    :rtype: int
+    """
+
+    print("adding!")
+    c = a + b
+
+    print("did add")
+    return c
