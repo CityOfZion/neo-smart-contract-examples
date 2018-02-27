ef52f50ea513d1c5d7fe681a470e0992be2345a8
Travis Lin <travis@localhost.dev>
2018-02-27 11:48:07 +1100

    Comment doc update

--- examples/python/01-constant/forty-two.py
+++ examples/python/01-constant/forty-two.py
@@ -1,13 +1,18 @@
 """
 License: MIT
 Author: Travis Lin
-Version: 1
-Contract Hash: 7eda0d8656310d97d6b342cd831f8d1be7186ee8
 Date Created: 2018-02-27
 Date Modified: 2018-02-27
-Example Build Test Command: `build ./examples/python/01-constant/forty-two.py test ff 02 False False`
-Example Import Command: `import contract ./examples/python/01-constant/forty-two.py ff 02 False False`
-Example Invoke: `testinvoke 7eda0d8656310d97d6b342cd831f8d1be7186ee8`
+Version: 1
+Contract Hash: d01b5d83dcecd6c6f19ac9c11c0a47588fe5abd7
+Available on NEO TestNet: False
+Available on CoZ TestNet: False
+Available on MainNet: False
+Example Build Test Command: `build /path/to/forty-two.py test ff 02 False False`
+Example Import Command: `import contract /path/to/forty-two.avm ff 02 False False`
+Example Invoke: `testinvoke d01b5d83dcecd6c6f19ac9c11c0a47588fe5abd7`
+Estimate Operation Count: 49
+Estimate GAS Consumption: 0.044
 """
 
 
--- examples/python/02-number/add.py
+++ examples/python/02-number/add.py
@@ -1,13 +1,18 @@
 """
 License: MIT
 Author: Travis Lin
-Version: 1
-Contract Hash: 28dfdb9d1d0fa0b86804e4a4e0f0c42484f32792
 Date Created: 2018-02-27
 Date Modified: 2018-02-27
-Example Build Test Command: `build ./examples/python/02-number/add.py test 0505 02 False False 2 5`
-Example Import Command: `import contract ./examples/python/02-number/add.py 0505 02 False False`
+Version: 1
+Contract Hash: 28dfdb9d1d0fa0b86804e4a4e0f0c42484f32792
+Available on NEO TestNet: False
+Available on CoZ TestNet: False
+Available on MainNet: False
+Example Build Test Command: `build /path/to/add.py test 0505 02 False False 2 5`
+Example Import Command: `import contract /path/to/add.avm 0505 02 False False`
 Example Invoke: `testinvoke 28dfdb9d1d0fa0b86804e4a4e0f0c42484f32792 2 5`
+Estimate Operation Count: 11
+Estimate GAS Consumption: 0.016
 """
 
 
