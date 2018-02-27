85ab3b22f1e1e592a0b84ba7c5d8ba824420a55f
Travis Lin <travis@localhost.dev>
2018-02-27 11:17:00 +1100

    Proper comment doc for python examples

--- examples/python/01-constant/fourty-two.py
+++ examples/python/01-constant/fourty-two.py
@@ -1,13 +1,19 @@
-
 """
-Test Command:
-    build ./contracts/python/01-constant/fourty-two.py test ff 02 True True
-
-Import Command:
-    import contract ./contracts/python/01-constant/fourty-two.py ff 02 True True
-
-Example Invocation:
-    testinvoke 7eda0d8656310d97d6b342cd831f8d1be7186ee8
+License: MIT
+Author: Travis Lin
+Version: 1
+Contract Hash: 7eda0d8656310d97d6b342cd831f8d1be7186ee8
+Date Created: 2018-02-27
+Date Modified: 2018-02-27
+Example Build Command: `./contracts/python/01-constant/fourty-two.py test ff 02 False False`
+Example Import Command: `import contract ./contracts/python/01-constant/fourty-two.py ff 02 False False`
+Example Invoke: `testinvoke 7eda0d8656310d97d6b342cd831f8d1be7186ee8`
 """
+
+
 def Main():
-    return 42
+    """
+    :return: Static number value.
+    :rtype: int
+    """
+    return 42  # Direct return an integer without usage of local variable
--- examples/python/02-number/add.py
+++ examples/python/02-number/add.py
@@ -1,13 +1,24 @@
+"""
+License: MIT
+Author: Travis Lin
+Version: 1
+Contract Hash: 28dfdb9d1d0fa0b86804e4a4e0f0c42484f32792
+Date Created: 2018-02-27
+Date Modified: 2018-02-27
+Example Build Command: `./contracts/python/02-number/add.py test 0505 02 False False 2 5`
+Example Import Command: `import contract ./contracts/python/02-number/add.py 0505 02 False False`
+Example Invoke: `testinvoke 28dfdb9d1d0fa0b86804e4a4e0f0c42484f32792 2 5`
+"""
+
+
 def Main(a, b):
     """
-    :param a: an integer
-    :param b: an integer
-    :return: a + b
+    :param a: First input number of concern
+    :param b: Second input number of concern
+    :type a: int
+    :type a: int
+    :return: Sum of the 2 input numbers
     :rtype: int
     """
-
-    print("adding!")
-    c = a + b
-
-    print("did add")
+    c = a + b  # It is a good practice to always allocate calculated value to a local variable instead of chaining to return or another function
     return c
