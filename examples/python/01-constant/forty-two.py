3a18f442cb105644d215051eeba918c4f6b0d876
Travis Lin <travis@localhost.dev>
2018-02-27 11:30:41 +1100

    Filename and comment correction

--- examples/python/01-constant/forty-two.py
+++ examples/python/01-constant/forty-two.py
@@ -5,8 +5,8 @@ Version: 1
 Contract Hash: 7eda0d8656310d97d6b342cd831f8d1be7186ee8
 Date Created: 2018-02-27
 Date Modified: 2018-02-27
-Example Build Command: `./contracts/python/01-constant/fourty-two.py test ff 02 False False`
-Example Import Command: `import contract ./contracts/python/01-constant/fourty-two.py ff 02 False False`
+Example Build Test Command: `build ./examples/python/01-constant/forty-two.py test ff 02 False False`
+Example Import Command: `import contract ./examples/python/01-constant/forty-two.py ff 02 False False`
 Example Invoke: `testinvoke 7eda0d8656310d97d6b342cd831f8d1be7186ee8`
 """
 
--- examples/python/02-number/add.py
+++ examples/python/02-number/add.py
@@ -5,8 +5,8 @@ Version: 1
 Contract Hash: 28dfdb9d1d0fa0b86804e4a4e0f0c42484f32792
 Date Created: 2018-02-27
 Date Modified: 2018-02-27
-Example Build Command: `./contracts/python/02-number/add.py test 0505 02 False False 2 5`
-Example Import Command: `import contract ./contracts/python/02-number/add.py 0505 02 False False`
+Example Build Test Command: `build ./examples/python/02-number/add.py test 0505 02 False False 2 5`
+Example Import Command: `import contract ./examples/python/02-number/add.py 0505 02 False False`
 Example Invoke: `testinvoke 28dfdb9d1d0fa0b86804e4a4e0f0c42484f32792 2 5`
 """
 
