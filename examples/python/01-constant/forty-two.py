86564d5555441f9e24774e61d1c9835188f5c85d
Travis Lin <travis@localhost.dev>
2018-02-27 12:05:30 +1100

    Add scaffold examples and documents

---
+++ examples/python/01-constant/forty-two.py
@@ -0,0 +1,24 @@
+"""
+License: MIT
+Author: Travis Lin
+Date Created: 2018-02-27
+Date Modified: 2018-02-27
+Version: 1
+Contract Hash: d01b5d83dcecd6c6f19ac9c11c0a47588fe5abd7
+Available on NEO TestNet: False
+Available on CoZ TestNet: False
+Available on MainNet: False
+Example Build Test Command: `build /path/to/forty-two.py test ff 02 False False`
+Example Import Command: `import contract /path/to/forty-two.avm ff 02 False False`
+Example Invoke: `testinvoke d01b5d83dcecd6c6f19ac9c11c0a47588fe5abd7`
+Estimate Operation Count: 49
+Estimate GAS Consumption: 0.044
+"""
+
+
+def Main():
+    """
+    :return: Static number value.
+    :rtype: int
+    """
+    return 42  # Direct return an integer without usage of local variable
